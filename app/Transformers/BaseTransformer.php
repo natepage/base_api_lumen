b9e64a54b82f61015eaa2dc148420f263360119c
NatePage <nathan.page@loyaltycorp.com.au>
2017-09-18 18:56:38 +1000

    [Transformers] added methods on BaseTransformer to include model and collections.

--- app/Transformers/BaseTransformer.php
+++ app/Transformers/BaseTransformer.php
@@ -2,6 +2,8 @@
 
 namespace App\Transformers;
 
+use App\Managers\ModelManagerInterface;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use League\Fractal\TransformerAbstract;
 
@@ -17,5 +19,38 @@ class BaseTransformer extends TransformerAbstract implements ModelTransformerInt
     public function transform(Model $model)
     {
         return $model->toArray();
+    }
+
+    /**
+     * Include one model as item.
+     *
+     * @param Model $model
+     *
+     * @return \League\Fractal\Resource\Item
+     */
+    protected function includeItem(Model $model)
+    {
+        /** @var ModelManagerInterface $modelManager */
+        $modelManager = clone app(ModelManagerInterface::class);
+        $modelManager->setModel($model);
+
+        return $this->item($model, $modelManager->getTransformer(), $modelManager->getModelKey());
+    }
+
+    /**
+     * Include collection of model as collection.
+     *
+     * @param string     $modelClass
+     * @param Collection $collection
+     *
+     * @return \League\Fractal\Resource\Collection
+     */
+    protected function includeCollection(string $modelClass, Collection $collection)
+    {
+        /** @var ModelManagerInterface $modelManager */
+        $modelManager = clone app(ModelManagerInterface::class);
+        $modelManager->setModel(new $modelClass());
+
+        return $this->collection($collection, $modelManager->getTransformer(), $modelManager->getModelKey());
     }
 }
